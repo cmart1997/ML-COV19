42c63177ca307c80818e7adc0e826c6219f8b1bc
cmart1997 <christmartinez1997@yahoo.com>
2020-04-20 03:06:58 -0700

    added percentage change/ stats
    added death percentage change per day

--- run.py
+++ run.py
@@ -3,24 +3,38 @@ import numpy as np
 import pandas as pd
 from scipy.stats import norm
 import csv
-import io
-import requests
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-sns.set()
-
 url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv"
-
-# s=requests.get(url).content
-# df=pd.read_csv(io.StringIO(s.decode('utf-8')))
 df=pd.read_csv(url)
-# fig,ax = plt.subplots()
 
-x =[]
-y = []
-for index,row in df.iterrows():
-    x.append(row["date"])
-    y.append(row["deaths"])
-plt.scatter(x,y)
+the_date =[]
+total_deaths = []
+percentage = []
+cases = []
+total_cases=[]
+differences = []
+current,current_counts = 0, 0
+row_iterator= df.iterrows()
+
+for index,row in row_iterator:
+    the_date.append(row["date"])
+    total_deaths.append(row["deaths"])
+    cases.append(row["cases"] - current_counts)
+    differences.append(row["deaths"] - current)
+    if current == 0:
+        difference = 0
+    else:
+        difference = ((row["deaths"] - current)/current)*100
+    percentage.append(difference)
+    current = row["deaths"]
+    current_counts = row["cases"]
+
+for i in range(0,len(the_date)):
+    print(f"On {the_date[i]}, {cases[i]} new cases, {differences[i]} new death(s).")
+    print(f"There has now been a total of {total_deaths[i]} deaths in the USA")
+    print(f"Which is {'%.2f' % percentage[i]}% more deaths than the day before. \n")
+
+plt.scatter(range(0,len(total_deaths)),total_deaths)
 plt.show()
